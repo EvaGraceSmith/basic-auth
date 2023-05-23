0c5686d8fa1265189e47615c12ed46052646af88
EvaGraceSmith <smithevagrace@gmail.com>
2023-05-22 23:59:57 -0500

    updated and added files

---
+++ src/server.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+
+app.get('/', (req, res, next) => {
+  res.status(200).send('proof of life');
+});
+
+
+const start = (port) => {
+  app.listen(port, () => console.log('server running on', port));
+};
+
+module.exports = {
+  app,
+  start,
+};
