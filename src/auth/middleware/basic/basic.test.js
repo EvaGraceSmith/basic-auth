7646de6b6c28a4a8823ee4297154db882a6a74fb
EvaGraceSmith <smithevagrace@gmail.com>
2023-05-23 21:26:41 -0500

    added tests to basic

--- src/auth/middleware/basic/basic.test.js
+++ src/auth/middleware/basic/basic.test.js
@@ -5,18 +5,32 @@
 // Does the middleware function (send it a basic header).
 // Do the routes assert the requirements (signup/signin).
 
+'use strict';
+
+const supertest = require('supertest');
+const { app } = require('../src/server');
+const { sequelize } = require('../src/auth/models');
+
+const request = supertest(app);
+
+beforeAll(async () => {
+  await sequelize.sync();
+},
+);
+
+
 test ('POST to /signup to create a new user', async () => {
-    const response = await request.post('/signup').send({username: 'test', password: 'test'});
-    expect(response.status).toEqual(201);
-    expect(response.body.username).toEqual('test');
-    }
+  const response = await request.post('/signup').send({username: 'test', password: 'test'});
+  expect(response.status).toEqual(201);
+  expect(response.body.username).toEqual('test');
+},
 );
 
 test ('POST to /signin to login as a user (use basic auth)', async () => {
-    const response = await request.post('/signin').auth('test', 'test');
-    expect(response.status).toEqual(200);
-    expect(response.body.username).toEqual('test');
-    }
+  const response = await request.post('/signin').auth('test', 'test');
+  expect(response.status).toEqual(200);
+  expect(response.body.username).toEqual('test');
+},
 );
 
 
